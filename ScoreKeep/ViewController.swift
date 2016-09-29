01dfb39c939a9882e4a834c4c093b1067d81996b
John Baker <baker@johns-macbook-pro.local>
2016-09-28 21:53:46 -0400

    Initial Commit

---
+++ ScoreKeep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScoreKeep
+//
+//  Created by John Baker on 9/28/16.
+//  Copyright © 2016 B4k3R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
